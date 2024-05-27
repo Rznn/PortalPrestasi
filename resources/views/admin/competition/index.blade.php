2d45d3926ab8266b2b7605970b86752222397095
Rznn <rafirzan24@gmail.com>
2024-05-27 09:23:16 +0700

    delete save change

--- resources/views/admin/competition/index.blade.php
+++ resources/views/admin/competition/index.blade.php
@@ -305,10 +305,6 @@
                                                                 data-hs-overlay="#hs-slide-up-animation-modal{{ $competition->id }}">
                                                                 Close
                                                             </button>
-                                                            <button type="submit"
-                                                                class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
-                                                                Save changes
-                                                            </button>
                                                         </div>
                                                     </div>
                                                 </div>
